1ce7cdeed1b497dcfa0dfd6d19706c4c733867e5
ק๏ ๏ђ <pooh1peach2@gmail.com>
2026-03-08 16:49:00 +0700

    Refactor LinkedList to StudentList with improvements

--- lab9_/9.1.c
+++ lab9_/9.1.c
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 
 using namespace std;
-
 
 struct Student {
     string name;
@@ -12,18 +12,19 @@ struct Student {
     float gpa;
     Student* next;
 
-    Student(string n, int a, char s, float g) : name(n), age(a), sex(s), gpa(g), next(NULL) {}
+    Student(string n, int a, char s, float g) 
+        : name(n), age(a), sex(s), gpa(g), next(nullptr) {}
 };
 
-class LinkedList {
+class StudentList {
 private:
     Student* head;
 
 public:
-    LinkedList() : head(NULL) {}
+    StudentList() : head(nullptr) {}
 
-    void addNode(string n, int a, char s, float g) {
-        Student* newNode = new Student(n, a, s, g);
+    void add(string name, int age, char sex, float gpa) {
+        Student* newNode = new Student(name, age, sex, gpa);
         if (!head) {
             head = newNode;
         } else {
@@ -33,38 +34,28 @@ public:
         }
     }
 
-
-    void showAll() {
+    Student* find(string name) {
         Student* temp = head;
         while (temp) {
-            cout << temp->name << " | " << temp->age << " | " 
-                 << temp->sex << " | " << temp->gpa << endl;
+            if (temp->name == name) return temp;
             temp = temp->next;
         }
+        return nullptr;
     }
 
-    
-    Student* findNode(string n) {
-        Student* temp = head;
-        while (temp) {
-            if (temp->name == n) return temp;
-            temp = temp->next;
-        }
-        return NULL;
-    }
-
-    
-    bool deleteNode(string n) {
+    bool remove(string name) {
         if (!head) return false;
-        if (head->name == n) {
+        
+        if (head->name == name) {
             Student* temp = head;
             head = head->next;
             delete temp;
             return true;
         }
+
         Student* curr = head;
-        while (curr->next && curr->next->name != n) curr = curr->next;
-        
+        while (curr->next && curr->next->name != name) curr = curr->next;
+
         if (curr->next) {
             Student* temp = curr->next;
             curr->next = curr->next->next;
@@ -74,7 +65,16 @@ public:
         return false;
     }
 
-    ~LinkedList() {
+    void display() {
+        Student* temp = head;
+        cout << "\n--- Student List ---\n";
+        while (temp) {
+            cout << temp->name << " (" << temp->age << "y, " << temp->sex << ") GPA: " << temp->gpa << endl;
+            temp = temp->next;
+        }
+    }
+
+    ~StudentList() {
         while (head) {
             Student* temp = head;
             head = head->next;
@@ -82,3 +82,33 @@ public:
         }
     }
 };
+
+int main() {
+    StudentList list;
+    int menu;
+
+    do {
+        cout << "\n1.Add 2.Delete 3.Find 4.Show 0.Exit: ";
+        cin >> menu;
+
+        if (menu == 1) {
+            string n; int a; char s; float g;
+            cout << "Name Age Sex GPA: ";
+            cin >> n >> a >> s >> g;
+            list.add(n, a, s, g);
+        } else if (menu == 2) {
+            string n; cout << "Name to delete: "; cin >> n;
+            if (list.remove(n)) cout << "Deleted.\n";
+            else cout << "Not found.\n";
+        } else if (menu == 3) {
+            string n; cout << "Name to find: "; cin >> n;
+            Student* s = list.find(n);
+            if (s) cout << "Found: " << s->name << " GPA: " << s->gpa << endl;
+            else cout << "Not found.\n";
+        } else if (menu == 4) {
+            list.display();
+        }
+    } while (menu != 0);
+
+    return 0;
+}
